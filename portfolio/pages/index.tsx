2877ca8cfca076d03f2b9e487052e86b725bb4c6
Yahn do Amaral Almeida Pinheiro <yahn.pinheiro@terceiroslinx.com.br>
2022-09-23 10:01:22 -0300

    Header section

--- portfolio/pages/index.tsx
+++ portfolio/pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Header from '../componets/Header'
+import Header from '../components/Header'
+import Hero from '../components/Hero'
 
 const Home: NextPage = () => {
   return (
@@ -13,6 +14,7 @@ const Home: NextPage = () => {
       <Header />
 
       {/* Hero */}
+      
 
       {/* About */}
 
